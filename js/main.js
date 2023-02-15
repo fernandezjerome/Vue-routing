166f9a73932d12ecd2db885fb1237e6d7f8e3662
fernandezjerome <fernandezjerome0824@gmail.com>
2023-02-15 16:19:56 -0500

    aaa

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //import your modules /components here
 import LoginPage from "./modules/LoginPage.js";
 import HomePage from "./modules/HomePage.js";
+import ErrorPage from "./modules/ErrorPage.js";
 
   
 const router = VueRouter.createRouter({
@@ -20,12 +21,25 @@ const router = VueRouter.createRouter({
         name: 'homme', // the name  of the route 
         component: HomePage // the component to render
     
+    },
+    { 
+        path: '/:pathMatch(.*)*', //the location bar url
+        name: 'error', // the name  of the route 
+        component: ErrorPage // the component to render
+    
     }
+
     ] // short for `routes: routes`
   })
   
   // 5. Create and mount the root instance.
-  const app = Vue.createApp()
+  const app = Vue.createApp({
+    method: {
+        tryRouterPush() {
+            this.$router.push({ name: 'home'})
+        }
+    }
+  })
   // Make sure to _use_ the router instance to make the
   // whole app router-aware.
   app.use(router)
--- js/modules/ErrorPage.js
+++ js/modules/ErrorPage.js
@@ -1,3 +1,12 @@
 export default {
-    template: `<h1>That page doesn't exist!</h1>`
+    template: `<h1>That page doesn't exist!</h1>`,
+    created () {
+        console.log('hit the router')
+
+
+        //redirect back to a safe route
+        setTimeout(()=> {
+            this.$router.push ({ name: 'home'});
+        }, 2500) //wait 2.5 sec.
+    }
 }
